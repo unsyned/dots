bf5d408372c6587449c59a94f7d8bf108c47660d
sy stroud <stroudafk@gmail.com>
2025-11-24 21:23:17 -0600

    Nvim theme

--- nvim/dot-config/nvim/lua/plugins/lualine.lua
+++ nvim/dot-config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@
 return
 {
       "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
+      dependencies = { "nvim-tree/nvim-web-devicons", "rebelot/kanagawa.nvim"},
       lazy = false,
       config = function()
         require("lualine").setup({
