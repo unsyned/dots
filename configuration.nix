cb09a2b7ae63414d3e0cc0eef3d860d8ca14d1a6
sy <stroudafk@gmail.com>
2026-04-25 11:27:44 -0500

    enable hardware acceleration?

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,9 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Hardware acceleration
+  hardware.graphics.enable = true;
+
   # Enable networking
   networking.networkmanager.enable = true;
 
